315fdd4f3b9f5ea5b22512c16acdcd88bf7e6f5d
Aniket Kumar <79148315+k-aniket47@users.noreply.github.com>
2022-08-24 03:25:07 +0530

    Update action-visitor.php

--- action-visitor.php
+++ action-visitor.php
@@ -28,21 +28,19 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Apartment Management System</title>
-  <!-- Tell the browser to be responsive to screen width -->
+  
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
+
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
+  
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
+
   <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
+
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
+
   <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
 
-  <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
   <script>
@@ -52,7 +50,7 @@
         url: "autofill.php",
         data:'apartmentid='+val,
         success: function(data){
-        //alert(data);
+
         $('#buildingno').val(data);
         }
         });
@@ -67,13 +65,12 @@
   
     <?php $page='visitor-management'; include 'includes/sidebar.php'?>
 
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+  
     <section class="content-header">
       <h1>
         Tenant's Entry Form
-        <!-- <small>Control panel</small> -->
+     
       </h1>
       <ol class="breadcrumb">
         <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -81,9 +78,9 @@
       </ol>
     </section>
 
-    <!-- Main content -->
+
     <section class="content">
-      <!-- Small boxes (Stat box) -->
+   
       
       <?php if($msg){ echo "<div class='alert alert-success alert-dismissible'>
                 <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
@@ -91,7 +88,7 @@
                 $msg
     </div>";}  ?>
 
-         <!-- Forms -->
+    
      
       
         <div class="box box-default">
@@ -102,7 +99,7 @@
               <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
             </div>
           </div>
-          <!-- /.box-header -->
+
 
           <?php
             $eid=$_GET['editid'];
@@ -138,9 +135,9 @@
                         <label>Tenant's  Entry Date and Time</label>
                         <input type="text" class="form-control" name="reason" id="reason" value="<?php  echo $row['EnterDate'];?>" readonly>
                     </div>
-                  <!-- /.form-group -->
+
                 </div>
-                <!-- /.col -->
+ 
                 <div class="col-md-6">
                     <div class="form-group">
                         <label>Tenant's Contact Number</label>
@@ -159,10 +156,6 @@
 
                   
 
-                    
-                  <!-- /.form-group -->
-                  
-                  <!-- /.form-group -->
                 </div>
 
                 <?php if($row['remark']==""){ ?>
@@ -189,13 +182,13 @@
                 </div>
 
                 <?php } }?>
-                <!-- /.col -->
+
               </div>
 
               
-              <!-- /.row -->
+
             </div>
-            <!-- /.box-body -->
+
             
           </div>
           </form>
@@ -229,48 +222,48 @@
  
     </div>
   </aside>
-  <!-- /.control-sidebar -->
+
 
   <div class="control-sidebar-bg"></div>
 </div>
-<!-- ./wrapper -->
 
-<!-- jQuery 3 -->
+
+
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
+
 <script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-<!-- Bootstrap 3.3.7 -->
+
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
+
 <script src="bower_components/raphael/raphael.min.js"></script>
 <script src="bower_components/morris.js/morris.min.js"></script>
-<!-- Sparkline -->
+
 <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
+
 <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
+
 <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
+
 <script src="bower_components/moment/min/moment.min.js"></script>
 <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
+
 <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
+
 <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
+
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
+
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
+
 <script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+
 <script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
+
 <script src="dist/js/demo.js"></script>
 </body>
 </html>
