bf6106f7d59ef3dd3e8eaff6cb113fb77fe0a994
Aniket Kumar <79148315+k-aniket47@users.noreply.github.com>
2022-08-24 03:28:30 +0530

    Update change-password.php

--- change-password.php
+++ change-password.php
@@ -28,18 +28,17 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Apartment Management System</title>
-  <!-- Tell the browser to be responsive to screen width -->
+ 
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
+
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
+
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
+
   <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
+
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
+
   <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
 
   <!-- Google Font -->
@@ -64,13 +63,12 @@
   
     <?php include 'includes/sidebar.php'?>
 
-  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+
     <section class="content-header">
       <h1>
         Change Password
-        <!-- <small>Control panel</small> -->
+
       </h1>
       <ol class="breadcrumb">
         <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -78,9 +76,9 @@
       </ol>
     </section>
 
-    <!-- Main content -->
+
     <section class="content">
-      <!-- Small boxes (Stat box) -->
+
       
       <?php if($msg){ echo "<div class='alert alert-success alert-dismissible'>
                 <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>
@@ -88,7 +86,7 @@
                 $msg
                 </div>";}  ?>
 
-         <!-- Forms -->
+
                     <?php
             $adminid=$_SESSION['avmsaid'];
             $ret=mysqli_query($con,"SELECT * from tbladmin where ID='$adminid'");
@@ -103,8 +101,7 @@
               <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
             </div>
           </div>
-          <!-- /.box-header -->
-          
+
 
             <div class="box-body">
               <div class="row">
@@ -115,10 +112,7 @@
                     <label>Account Registered At</label>
                     <input type="text" class="form-control" value="<?php  echo $row['AdminRegdate'];?>" readonly>
                   </div>
-                  <!-- /.form-group -->
 
-                    
-                  <!-- /.form-group -->
                 </div>
                 <!-- /.col -->
                 <div class="col-md-3">
@@ -146,41 +140,32 @@
                 </div>
                 
                 </div>
-            <!-- /.col --> <?php }?>
+ <?php }?>
               </div>
 
               
-              <!-- /.row -->
+
             </div>
-            <!-- /.box-body -->
+
             <div class="box-footer">
             <button type="submit" class="btn btn-block btn-danger btn-lg" name="update">Make Changes</button>
             </div>
           </div>
           </form>
-      
-      <!-- /Form -->
-        
-    
-	  
-      <!-- Main row -->
-      
-      <!-- / Main row -->
+
 
     </section>
-    <!-- /.content -->
+
   </div>
-  <!-- /.content-wrapper -->
+
   
   <?php include 'includes/footer.php'?>
 
-  <!-- Control Sidebar -->
+
   <aside class="control-sidebar control-sidebar-dark" style="display: none;">
-    <!-- Create the tabs -->
-    
-    <!-- Tab panes -->
+
     <div class="tab-content">
-      <!-- Home tab content -->
+
 
       <div class="tab-pane" id="control-sidebar-home-tab">
        
@@ -188,48 +173,48 @@
  
     </div>
   </aside>
-  <!-- /.control-sidebar -->
+
 
   <div class="control-sidebar-bg"></div>
 </div>
-<!-- ./wrapper -->
 
-<!-- jQuery 3 -->
+
+
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
+
 <script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-<!-- Bootstrap 3.3.7 -->
+
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
+
 <script src="bower_components/raphael/raphael.min.js"></script>
 <script src="bower_components/morris.js/morris.min.js"></script>
-<!-- Sparkline -->
+
 <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
+
 <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
+
 <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
+
 <script src="bower_components/moment/min/moment.min.js"></script>
 <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
+
 <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
+
 <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
+
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
+
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
+
 <script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+
 <script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
+
 <script src="dist/js/demo.js"></script>
 </body>
 </html>
