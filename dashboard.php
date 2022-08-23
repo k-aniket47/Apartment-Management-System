9a9b972520bbf4b4a2c350d897ee1ede2bba3896
Aniket Kumar <79148315+k-aniket47@users.noreply.github.com>
2022-08-24 03:31:44 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -14,21 +14,19 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Apartment Management System</title>
-  <!-- Tell the browser to be responsive to screen width -->
+  
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
+
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
+
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
+
   <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
+
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
+  
   <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
 
-  <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
@@ -38,13 +36,13 @@
   
     <?php $page='dashboard'; include 'includes/sidebar.php'?>
 
-  <!-- Content Wrapper. Contains page content -->
+
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+
     <section class="content-header">
       <h1>
         Dashboard
-        <!-- <small>Control panel</small> -->
+     
       </h1>
       <ol class="breadcrumb">
         <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -52,12 +50,12 @@
       </ol>
     </section>
 
-    <!-- Main content -->
+
     <section class="content">
-      <!-- Small boxes (Stat box) -->
+
       <div class="row">
         <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
+
           <div class="small-box bg-primary">
             <div class="inner">
               <h3><?php include 'counters/total-visitor-count.php'?></h3>
@@ -70,9 +68,9 @@
             <a href="visitor-mgmt.php" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-        <!-- ./col -->
+
         <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
+
           <div class="small-box bg-green">
             <div class="inner">
               <h3><?php include 'counters/todays-visitor-count.php'?></h3>
@@ -85,9 +83,9 @@
             <a href="visitor-mgmt.php" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-        <!-- ./col -->
+
         <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
+
           <div class="small-box bg-yellow">
             <div class="inner">
               <h3><?php include 'counters/occ-apartment.php'?></h3>
@@ -100,9 +98,9 @@
             <a href="manage-apartment.php" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-        <!-- ./col -->
+
         <div class="col-lg-3 col-xs-6">
-          <!-- small box -->
+
           <div class="small-box bg-red">
             <div class="inner">
               <h3><?php include 'counters/av-apartment.php'?></h3>
@@ -115,31 +113,24 @@
             <a href="manage-apartment.php" class="small-box-footer">More Info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
-        <!-- ./col -->
-      </div>
-      <!-- /.row -->
 
-      
-	  
-      <!-- Main row -->
-      
-      <!-- / Main row -->
+      </div>
+
+
 
     </section>
-    <!-- /.content -->
+
   </div>
-  <!-- /.content-wrapper -->
-  <!-- Developed by Naseeb Bajracharya -->
+
   
   <?php include 'includes/footer.php'?>
 
-  <!-- Control Sidebar -->
+
   <aside class="control-sidebar control-sidebar-dark" style="display: none;">
-    <!-- Create the tabs -->
-    
-    <!-- Tab panes -->
+
+
     <div class="tab-content">
-      <!-- Home tab content -->
+
 
       <div class="tab-pane" id="control-sidebar-home-tab">
        
@@ -151,45 +142,42 @@
 
   <div class="control-sidebar-bg"></div>
 </div>
-<!-- ./wrapper -->
 
-<!-- jQuery 3 -->
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
+
 <script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<!-- Developed by Naseeb Bajracharya -->
+
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-<!-- Bootstrap 3.3.7 -->
+
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
+
 <script src="bower_components/raphael/raphael.min.js"></script>
 <script src="bower_components/morris.js/morris.min.js"></script>
-<!-- Sparkline -->
+
 <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
+
 <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
+
 <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
+
 <script src="bower_components/moment/min/moment.min.js"></script>
 <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
+
 <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
+
 <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
+
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
+
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
+
 <script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+
 <script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
+
 <script src="dist/js/demo.js"></script>
 </body>
 </html>
