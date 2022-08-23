8fb9cf83227074065d541fb901d5f40d42376c7e
Aniket Kumar <79148315+k-aniket47@users.noreply.github.com>
2022-08-24 03:23:41 +0530

    Update visitor-mgmt.php

--- visitor-mgmt.php
+++ visitor-mgmt.php
@@ -14,27 +14,26 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>Apartment Management System</title>
-  <!-- Tell the browser to be responsive to screen width -->
+  
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.7 -->
+
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
-  <!-- Font Awesome -->
+
   <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  <!-- Ionicons -->
+  
   <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
-  <!-- Theme style -->
+
 
   <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-  <!-- AdminLTE Skins. Choose a skin from the css/skins
-       folder instead of downloading all of them to reduce the load. -->
+
   <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-  <!-- Morris chart -->
+
   <link rel="stylesheet" href="bower_components/morris.js/morris.css">
   
-  <!-- DataTables -->
+
   <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
 
-  <!-- Google Font -->
+
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
@@ -44,13 +43,13 @@
   
     <?php $page='visitor-management'; include 'includes/sidebar.php'?>
 
-  <!-- Content Wrapper. Contains page content -->
+
   <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
+
     <section class="content-header">
       <h1>
         Tenant Management
-        <!-- <small>Control panel</small> -->
+       
       </h1>
       <ol class="breadcrumb">
         <li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Home</a></li>
@@ -58,7 +57,7 @@
       </ol>
     </section>
 
-    <!-- Main content -->
+
     <section class="content">
      
     <div class="row">
@@ -68,7 +67,7 @@
             <div class="box-header">
               <h3 class="box-title">Displaying All Tenant's Entry</h3>
             </div>
-            <!-- /.box-header -->
+    
             <div class="box-body">
               <table id="example1" class="table table-bordered table-hover table-striped">
                 <thead>
@@ -121,27 +120,27 @@
               
               </table>
             </div>
-            <!-- /.box-body -->
+   
           </div>
-          <!-- /.box -->
+      
         </div>
-        <!-- /.col -->
+
       </div>
 
     </section>
-    <!-- /.content -->
+    
   </div>
-  <!-- /.content-wrapper -->
+
   
   <?php include 'includes/footer.php'?>
 
-  <!-- Control Sidebar -->
+ 
   <aside class="control-sidebar control-sidebar-dark" style="display: none;">
-    <!-- Create the tabs -->
+  
     
-    <!-- Tab panes -->
+
     <div class="tab-content">
-      <!-- Home tab content -->
+
 
       <div class="tab-pane" id="control-sidebar-home-tab">
        
@@ -149,53 +148,53 @@
  
     </div>
   </aside>
-  <!-- /.control-sidebar -->
+
 
   <div class="control-sidebar-bg"></div>
 </div>
-<!-- ./wrapper -->
 
-<!-- jQuery 3 -->
+
+
 <script src="bower_components/jquery/dist/jquery.min.js"></script>
-<!-- jQuery UI 1.11.4 -->
+
 <script src="bower_components/jquery-ui/jquery-ui.min.js"></script>
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
-<!-- Bootstrap 3.3.7 -->
+
 <script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- DataTables -->
+
 <script src="bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
 <script src="bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 
-<!-- Morris.js charts -->
+
 <script src="bower_components/raphael/raphael.min.js"></script>
 <script src="bower_components/morris.js/morris.min.js"></script>
-<!-- Sparkline -->
+
 <script src="bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
+
 <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
 <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
+
 <script src="bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
+
 <script src="bower_components/moment/min/moment.min.js"></script>
 <script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
+
 <script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
+
 <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
+
 <script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
+
 <script src="bower_components/fastclick/lib/fastclick.js"></script>
 
-<!-- AdminLTE App -->
+
 <script src="dist/js/adminlte.min.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+
 <script src="dist/js/pages/dashboard.js"></script>
-<!-- AdminLTE for demo purposes -->
+
 <script src="dist/js/demo.js"></script>
 
 <script>
